4f266093231f1c10e864b9faf49cae4d175885c9
jx06t <50313tjx06@gmail.com>
2026-05-04 00:55:18 +0800

    feat: enhance Partners section with link support and visual indicators for mobile

--- src/data/partners.ts
+++ src/data/partners.ts
@@ -6,10 +6,10 @@ export interface Partner {
 }
 
 export const PARTNERS: Partner[] = [
-  { name: "建中電子研究社", role: "主辦" },
-  { name: "建中資訊社", role: "協辦" },
-  { name: "成功電子研究社", role: "協辦" },
-  { name: "北一女資訊研究社", role: "主辦" },
-  { name: "中山女高資訊研究社", role: "協辦" },
-  { name: "景美電腦研究社", role: "協辦" },
+  { name: "建中電研", role: "主辦", url: "https://www.ckefgisc.org/" },
+  { name: "建中資訊", role: "協辦" },
+  { name: "成功電研", role: "協辦" },
+  { name: "北一資研", role: "主辦", url: "https://www.ckefgisc.org/" },
+  { name: "中山資研", role: "協辦" },
+  { name: "景美電研", role: "協辦" },
 ];
